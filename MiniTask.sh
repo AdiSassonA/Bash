b659c0eb54f251e7da12270b8d3322f8719da35d
AdiSassonA <42842617+AdiSassonA@users.noreply.github.com>
2019-05-30 13:35:59 +0300

    This is my answer to MiniTask https://github.com/BigRush/MiniTask/blob/master/task.txt

---
+++ MiniTask.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#create an array
+arr=()
+read -p "enter number: " num
+for i in $num; do
+	re='^[0-9]+$'
+	if ! [[ $i =~ $re ]] ; then
+   		echo "error: $i is not a number" >&2; exit 1
+	fi
+	while [[ $i -lt 0 ]]; do
+		echo "error: $i is a negative number" >&2; exit 1
+		exit 1
+	done
+	if [[ $i -gt 0 ]]; then
+		arr+=($i)
+	fi
+done
+max=${arr[0]}
+min=${arr[0]}
+len=${#arr[@]}
+# Loop through all elements in the array
+for y in "${arr[@]}"
+do
+	sum=$(( $sum + $y ))
+    # Update max if applicable
+	if [[ "$y" -gt "$max" ]]; then
+        max="$y"
+	fi
+
+    # Update min if applicable
+    	if [[ "$y" -lt "$min" ]]; then
+        min="$y"
+    	fi
+done
+#calculate the average
+avg=$(( $sum / $len ))
+# Output results:
+echo "Max is: $max"
+echo "Min is: $min"
+echo "Avg is: $avg"
